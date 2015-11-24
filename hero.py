38d7a9fdc3dc0005c1484c27fcb09e75ea82d755
abushaa <alishaabu@mail.ru>
2015-11-24 10:56:15 +0300

    Made a Hero

--- hero.py
+++ hero.py
@@ -2,6 +2,27 @@
 # license: GPLv3
 from gameunit import *
 
+
+class Hero(Attacker):
+    _experience=None
+    _name=None
+
+    def attack(self, target):
+        target._health -= self._attack
+
+
+    def __init__(self, name = 'Player1' ):
+        self._health = 100
+        self._attack = 50
+        self.experienxe = 0
+        self._name = name
+
+
+    def gameOver(self):
+            if self._health <= 0:
+                print('К сожалению, Вы проиграли...')
+
+
 #FIXME:
 """В этом файле должен быть описан класс героя, унаследованный от Attacker
 Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
